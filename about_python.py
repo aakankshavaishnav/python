8b7df3bf4e8a54f961bf84591cf3b4db2c5164e9
aakankshavaishnav <aakanksha.vaishnav.cg@gmail.com>
2026-08-23 15:39:57 +0530

    python basis

---
+++ about_python.py
@@ -0,0 +1,11 @@
+print("hello world")
+# Multiple assignment
+name, age = "Rahul", 18
+
+# Same value to multiple variables
+x = y = z = 0
+
+# Reassignment
+age = 18
+age = 19       # final value is 19
+print(name,age)

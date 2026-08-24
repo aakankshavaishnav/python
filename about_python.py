902115dd2b5fc2bfedcd0ba2c2973e6cf06a9c7d
aakankshavaishnav <aakanksha.vaishnav.cg@gmail.com>
2026-08-24 11:10:31 +0530

    demo updated

--- about_python.py
+++ about_python.py
@@ -5,7 +5,14 @@ name, age = "Rahul", 18
 # Same value to multiple variables
 x = y = z = 0
 
-# Reassignment
+# Reassignment  
 age = 18
 age = 19       # final value is 19
 print(name,age)
+
+print("hello world")
+is_student = True
+
+print(type(is_student))
+age = 18
+result = None
---
+++ demo.py
@@ -0,0 +1,16 @@
+age=17
+result=None
+name="Rahul"
+print(name,age,result)
+age = 18
+
+print(type(age))
+print("hello world")
+age = 18
+age_float = 18.5
+age_string = "18"
+age_boolean = True
+print(type(age))
+print(type(age_float))
+print(type(age_string))
+print(type(age_boolean))

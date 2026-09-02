f4ae0360e1c24c898d6fa8b9588f14a1be6717a0
aakankshavaishnav <aakanksha.vaishnav.cg@gmail.com>
2026-09-02 10:50:11 +0530

    boolean updated

---
+++ 06_boolean_and_logical/boolean.py
@@ -0,0 +1,15 @@
+print(5>3)
+a=10
+b=12
+print(a<b)
+c=202020
+d=939303030
+print(c>=d)
+name="ABC"
+print(name<="CBA")
+song="AA"
+print(song<"aA")
+print(True and True) 
+print(True and False) 
+print(False and True)
+print(False and False)

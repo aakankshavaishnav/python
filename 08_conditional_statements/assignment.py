4de05a31fa6b12c86a44d4465bb68574e1a316d4
aakankshavaishnav <aakanksha.vaishnav.cg@gmail.com>
2026-09-08 09:42:18 +0530

    updated

--- 08_conditional_statements/assignment.py
+++ 08_conditional_statements/assignment.py
@@ -1,119 +1,119 @@
-# age=int(input("enter your age: ").split()[0])
-# print(age)
-# if age>=18:
-#     print("true")
-# number=int(input("enetr your number: "))
-# if number>=0 :
-#     print("positive") 
+age=int(input("enter your age: ").split()[0])
+print(age)
+if age>=18:
+    print("true")
+number=int(input("enetr your number: "))
+if number>=0 :
+    print("positive") 
 
-# #Q=4
-# Result=int(input("eneter your marks"))
-# if Result>=40 :
-#     print("you are pass")
+#Q=4
+Result=int(input("eneter your marks"))
+if Result>=40 :
+    print("you are pass")
     
-#Q=5
-# user=int(input("enetr your number: "))
-# if user==0 :
-#     print("zero")
+Q=5
+user=int(input("enetr your number: "))
+if user==0 :
+    print("zero")
 
-# #Q=6
-# number=int(input("enetr youur number: "))
-# if number % 2 ==0 :
-#     print("number is even")
-# else:
-#     print("numbert is odd")
-# #Q=7
-# age=int(input("enter your age"))
-# if age>= 18 :
-#     print("Adult")
-# else:
-#     print("Minor")
+#Q=6
+number=int(input("enetr youur number: "))
+if number % 2 ==0 :
+    print("number is even")
+else:
+    print("numbert is odd")
+#Q=7
+age=int(input("enter your age"))
+if age>= 18 :
+    print("Adult")
+else:
+    print("Minor")
 
-# #Q=8
-# number=int(input("enetr your number  :"))
-# if number %2 ==0 :
-#     print("Even")
-# else:
-#     print("Odd")
+#Q=8
+number=int(input("enetr your number  :"))
+if number %2 ==0 :
+    print("Even")
+else:
+    print("Odd")
 
-# #Q=9
-# marks=int(input("enter your martks- "))
-# if marks>=40 :
-#     print("your are pass")
-# else:
-#     print("Fail")
+#Q=9
+marks=int(input("enter your martks- "))
+if marks>=40 :
+    print("your are pass")
+else:
+    print("Fail")
 
-# #Q=10
-# num1= int(input("enter your first number: "))
-# num2= int(input("enetr your number: "))
-# if num1> num2 :
-#     print("num1 is greater")
-# else:
-#     print("num2 is greater")
+#Q=10
+num1= int(input("enter your first number: "))
+num2= int(input("enetr your number: "))
+if num1> num2 :
+    print("num1 is greater")
+else:
+    print("num2 is greater")
 
-#Q=16
-# age=int(input("enter your age"))
-# if age>=18 :
-#     if age<=60 :
-#          print("your are above 18")
-#     else:print("belowe 18")
-# else: print("your are below 18")
+Q=16
+age=int(input("enter your age"))
+if age>=18 :
+    if age<=60 :
+         print("your are above 18")
+    else:print("belowe 18")
+else: print("your are below 18")
 
-# a=map(int ,input("enter your first num: "))
-# b=map(int ,input("enter your second num: "))
-# # c=int(input("enter your third number: "))
-# operator =input("+,-,*,% , //")
-# if operator=="+" :
-#     print(f"addition is :("a+b"))
+a=map(int ,input("enter your first num: "))
+b=map(int ,input("enter your second num: "))
+# c=int(input("enter your third number: "))
+operator =input("+,-,*,% , //")
+if operator=="+" :
+    print(f"addition is :("a+b"))
 
-# elif operator=="-" :
-#     print("a-b")    
+elif operator=="-" :
+    print("a-b")    
 
-# elif operator=="%" :
-#     print("a%b") 
+elif operator=="%" :
+    print("a%b") 
 
-# elif operator=="//" :
-#     print("a//b")  
+elif operator=="//" :
+    print("a//b")  
 
-# else: print("invalid operator")   
-# nested list
-
-
-# Age=int(input("enter your Age"))
-# if Age>=18 :
-#     print(your )
+else: print("invalid operator")   
+nested list
 
 
+Age=int(input("enter your Age"))
+if Age>=18 :
+    print(your )
 
-# age=int(input("enetr your age: ")) 
-# if age>=18 :
-#     print("your are above 18") 
-#     if age <=60 :
-#         print("you are atmost 60")
-#     else:
-#         print("but you ar eolder than 18") 
-# else:print("error")
 
-#Q=17
-# marks=int(input("enter your marks: "))
-# if marks>=40 :
-#     print("you are passed")
-#     if marks>=75 :
-#         print("good")
-#     elif marks<=45 :
-#         print("failed")
+
+age=int(input("enetr your age: ")) 
+if age>=18 :
+    print("your are above 18") 
+    if age <=60 :
+        print("you are atmost 60")
+    else:
+        print("but you ar eolder than 18") 
+else:print("error")
+
+Q=17
+marks=int(input("enter your marks: "))
+if marks>=40 :
+    print("you are passed")
+    if marks>=75 :
+        print("good")
+    elif marks<=45 :
+        print("failed")
 
         
-# else:print("invalid")
-# #Q=19
+else:print("invalid")
+#Q=19
 
-# Age=int(input("enter your value: "))
-# if Age>=18 :
-#     print("you are adult")
-#     if Age>= 60 :
-#         print("you are senior")
-#     else:print("your not super senior")
-# else:print("you are under 18")
+Age=int(input("enter your value: "))
+if Age>=18 :
+    print("you are adult")
+    if Age>= 60 :
+        print("you are senior")
+    else:print("your not super senior")
+else:print("you are under 18")
 
 #Q=20
 age= int(input("enter your age: "))
